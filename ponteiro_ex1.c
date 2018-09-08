88b377edf5a2eb172f2c32e5ef723b343bd36572
RobsondCCarneiro <43090615+RobsondCCarneiro@users.noreply.github.com>
2018-09-08 20:54:47 -0300

    Add files via upload

---
+++ ponteiro_ex1.c
@@ -0,0 +1,14 @@
+/*ESCOLA DE CIÊNCIAS E TECNOLOGIA
+DEPARTAMENTO DE ENGENHARIA DE COMPUTACAO E AUTOMACAO
+DCA1202 - PROGRAMAÇÃO AVANÇADA
+PROFESSOR: AGOSTINHO DE MEDEIROS BRITO JUNIOR
+ALUNO: ROBSON DA COSTA CARNEIRO
+LISTA 1: PONTEIROS
+
+
+Qual a utilidade do aprendizado do uso de ponteiro para aplicações em engenharia?
+R.: Ponteiros é quando a variável guarda o endereço ao invés do valor da variável,
+muito útil para quando é necessário quando implementa um programa que precisa de muitas
+funções, porque assim o valor continua salvo quando resgatar o valor em outra função que invocou a outra.
+A engenharia usa programas que são complexos e que precisam de muitas funções, então os ponteiros são úteis nesses casos.
+*/
